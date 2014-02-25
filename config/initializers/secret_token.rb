eb466f37497e0d7aa97a0ef814d2a672a0d002c5
Nick Ross <mx5433@gmail.com>
2014-02-25 13:47:05 -0600

    firat commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ed0571e7c38e4a77bb8ff85a1c9fa500b6e7afeee39afd35de0fdf5ae61e063c52960b66304f3c3c19511b5ec6f8243e101a5aa3c39c3c5909c4eb5d57a4de97'
